6db4956b72093e1be094a217b76b30209efb0288
Filipe Stefano <filipe.ghisi@gmail.com>
2025-02-01 00:47:30 -0300

    Added UI, game feedback, option to change difficulty.

--- scenes/state_machine/state_broken.gd
+++ scenes/state_machine/state_broken.gd
@@ -2,6 +2,10 @@ extends NodeState
 
 @export var lock: Lock
 @export var lock_secret: LockSecret
+@export var line_to_cursor: LineToAngle
+@export var broken_line_color: Color = Color('e9736bcc')
+
+signal broke_lock
 
 
 func _on_process(_delta : float) -> void:
@@ -17,11 +21,18 @@ func _on_next_transitions() -> void:
 
 
 func _on_enter() -> void:
-	print("Crack!")
-	await get_tree().create_timer(Constants.FANFARRE_DURATION).timeout
+	broke_lock.emit()
+	var exit_delay = Constants.FANFARRE_DURATION
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(line_to_cursor, "modulate", broken_line_color, exit_delay * 0.15)
+	tween.tween_property(line_to_cursor, "modulate:a", 0, exit_delay * 0.5)
+	
+	await get_tree().create_timer(exit_delay).timeout
 	transition.emit("Idle")
 
 
 func _on_exit() -> void:
-	lock_secret.randomize_secret()
+	line_to_cursor.modulate = Color.WHITE
+	lock_secret.fix_lock()
 	lock.cylinder_rotation = 0.0
